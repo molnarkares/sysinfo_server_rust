f827a6874ce9c90399079ac54f1ce8d0c9419f04
kares <molnarkares@gmail.com>
2023-10-23 14:15:09 +0200

    removed Unix specific user list handling

--- src/users.rs
+++ src/users.rs
@@ -8,16 +8,12 @@ use sysinfo::{System, SystemExt, UserExt};
 pub(crate) async fn handle_users(system: Arc<Mutex<System>>) -> Result<Response<Body>, hyper::Error> {
     let mut system = system.lock().unwrap();
     system.refresh_users_list();
-    let logged_in_users = _get_logged_in_users();
-
-    let users: Vec<_> = system.users().iter()
-        .filter(|user| logged_in_users.contains(&user.name().to_string()))
-        .map(|user| {
-            json!({
-                "name": user.name(),
-                "group": user.groups()
-            })
-        }).collect();
+    let users: Vec<_> = system.users().iter().map(|user| {
+        json!({
+            "name": user.name(),
+            "group": user.groups()
+        })
+    }).collect();
 
 
     let response = match Response::builder()
@@ -29,14 +25,3 @@ pub(crate) async fn handle_users(system: Arc<Mutex<System>>) -> Result<Response<
     Ok(response)
 }
 
-fn _get_logged_in_users() -> Vec<String> {
-    use std::process::Command;
-
-    let output = Command::new("who")
-        .arg("-q")
-        .output()
-        .expect("failed to execute `who` command");
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    stdout.lines().next().unwrap_or_default().split_whitespace().map(|s| s.to_string()).collect()
-}
