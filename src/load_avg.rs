40be5a0d4119e3c9c3b4796575afdc83c1208e23
kares <molnarkares@gmail.com>
2023-10-23 14:56:04 +0200

    cleanup on  load_average

--- src/load_avg.rs
+++ src/load_avg.rs
@@ -9,23 +9,13 @@ use sysinfo::{System, SystemExt};
 pub(crate) async fn handle_load_average(system: Arc<Mutex<System>>) -> Result<Response<Body>, hyper::Error> {
     let mut system = system.lock().unwrap();
     system.refresh_system();
-    // #[cfg(unix)]
         let load_average = system.load_average();
 
-    // #[cfg(unix)]
         let result = json!([{
         "one": load_average.one,
         "five": load_average.five,
         "fifteen": load_average.fifteen,
     }]);
-
-    // // For windows, we'll just return a dummy value.
-    // #[cfg(not(unix))]
-    //     let result = json!([{
-    //     "one": -1.0,
-    //     "five": -1.0,
-    //     "fifteen": -1.0,
-    // }]);
 
     let response = match Response::builder()
         .header("Content-Type", "application/json")
