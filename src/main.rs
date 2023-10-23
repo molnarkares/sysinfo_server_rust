6f55700af78a9f6e6a3152f9478706fe56271f27
kares <molnarkares@gmail.com>
2023-10-23 11:11:24 +0200

    added sysinfo

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use hyper::{Body, Request, Response, Server};
 use hyper::http::StatusCode;
 use hyper::service::{make_service_fn, service_fn};
 use serde_json::json;
-use sysinfo::{ComponentExt, System, SystemExt};
+use sysinfo::{ComponentExt, System, SystemExt, CpuExt, DiskExt};
 
 #[tokio::main]
 async fn main() {
@@ -133,6 +133,29 @@ async fn handle_request(req: Request<Body>, system: Arc<Mutex<System>>) -> Resul
             };
             Ok(response)
         }
+
+        "/sysinfo" => {
+            let mut system = system.lock().unwrap();
+            system.refresh_all();
+
+            let system_data = json!([{
+                "kernel_version": system.kernel_version().unwrap_or_else(|| "N/A".to_string()),
+                "os_version": system.os_version().unwrap_or_else(|| "N/A".to_string()),
+                "long_os_version": system.long_os_version().unwrap_or_else(|| "N/A".to_string()),
+                "distribution_id": system.distribution_id(),
+                "host_name": system.host_name().unwrap_or_else(|| "N/A".to_string()),
+            }]);
+
+            let response = match Response::builder()
+                .header("Content-Type", "application/json")
+                .body(Body::from(json!(system_data).to_string())) {
+                Ok(res) => res,
+                Err(_) => Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::empty()).unwrap(),
+            };
+            Ok(response)
+        }
+
+
 
         _ => {
             let response = Response::builder()
